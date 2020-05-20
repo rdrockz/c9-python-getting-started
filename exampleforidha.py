d9a87069c3b1ca06ecfe26ebe586ec40a15fa83a
rdrockz <rdrockz@users.noreply.github.com>
2020-05-20 17:35:34 +0530

    commit

---
+++ exampleforidha.py
@@ -0,0 +1,27 @@
+#%%
+# I am just writing a comment
+
+print(2+3-2*5)
+
+
+# %%
+y=1
+z=5
+x=z+y
+
+x="IdHa"
+y="Sharma"
+z="De"
+
+print((x+" " + y+ " " +z).upper())
+
+
+
+
+# %%
+i=0
+for x in range (101):
+    i=i+x
+    print(i)
+
+print(i)
